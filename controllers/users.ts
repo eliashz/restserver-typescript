8fb9e3c2bc47ab0fe8d92a6b21a3eb9a0cfa9ad9
eliashz <eliashz@gmail.com>
2023-02-16 17:40:38 +0100

    routes

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,6 +1,30 @@
 import { Request, Response } from "express";
 
-const getUsers = (req: Request, res: Response) => {
+export const getUsers = (req: Request, res: Response) => {
+  res.json({ msg: "getUsers" });
+};
 
-    
+export const getUser = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  res.json({ msg: "getUser", id });
+};
+
+export const postUser = (req: Request, res: Response) => {
+  const { body } = req;
+
+  res.json({ msg: "postUser", body });
+};
+
+export const putUser = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  res.json({ msg: "putUser", body, id });
+};
+
+export const deleteUser = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  res.json({ msg: "deleteUser", id });
 };
